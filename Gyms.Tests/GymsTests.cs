10b314f2eee5117eb25aae3eea2215b1530dd2ef
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-04 14:36:47 +0200

    C# OOP Exam - 11 December 2021

--- Gyms.Tests/GymsTests.cs
+++ Gyms.Tests/GymsTests.cs
@@ -1,7 +1,23 @@
-﻿namespace Gyms.Tests
+﻿using NUnit.Framework;
+
+namespace Gyms.Tests
 {
     public class GymsTests
     {
-        // Implement unit tests here
+        private Athlete athlete;
+        private Gym gym;
+
+        [SetUp]
+        public void Setup()
+        {
+            athlete = new Athlete("AthleteName");
+            gym = new Gym("GymName", 70);
+        }
+
+        [Test]
+        public void NameGetterWorks()
+        {
+            Assert.That(gym.Name, Is.EqualTo("GymName"));
+        }
     }
 }
